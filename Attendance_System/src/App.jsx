76f966cb01a2ee30d7867b6e25a8e716906d09f3
umar aurangzeb <umar12598@gmail.com>
2024-11-02 17:32:28 +0500

    build

--- Attendance_System/src/App.jsx
+++ Attendance_System/src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes,Route } from 'react-router-dom'
-import AttendanceForm from './components/attedanceInfo'
+import { Routes, Route } from 'react-router-dom'
+import AttendanceForm from './components/attendanceInfo'
 import SuccessMessage from './components/success'
 import NotFoundPage from './components/No_Found'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AttendanceForm/>}></Route>
-      <Route path="/success" element={<SuccessMessage/>}></Route>
-      <Route path="*" element={<NotFoundPage/>}></Route>
+      <Route path="/" element={<AttendanceForm />}></Route>
+      <Route path="/success" element={<SuccessMessage />}></Route>
+      <Route path="*" element={<NotFoundPage />}></Route>
     </Routes>
   )
 }
