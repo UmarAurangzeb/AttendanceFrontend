74da014ff7221c6cc2702029dc530a23f7d43346
umar aurangzeb <umar12598@gmail.com>
2024-11-02 17:29:09 +0500

    build

--- Attendance_System/src/components/No_Found.jsx
+++ Attendance_System/src/components/No_Found.jsx
@@ -1,21 +1,22 @@
 import '../Styles/errorPage.css'
+import React from 'react';
 import DeveloperDay from './toolTip';
 
 function NotFoundPage() {
-  return ( 
+  return (
     <>
-        <div className="MyContainer">
-          <div className="MyBox" style={{textAlign: "center", marginTop: "20px"}}>
-            <div className="h404" >404</div>
-            <div class="lead">Are you lost, little one?</div>
-          </div>
-          
+      <div className="MyContainer">
+        <div className="MyBox" style={{ textAlign: "center", marginTop: "20px" }}>
+          <div className="h404" >404</div>
+          <div class="lead">Are you lost, little one?</div>
         </div>
-        <div>
-        <DeveloperDay/>
+
+      </div>
+      <div>
+        <DeveloperDay />
       </div>
     </>
-   );
+  );
 }
 
 export default NotFoundPage;
--- Attendance_System/src/components/attedanceInfo.jsx
+++ Attendance_System/src/components/attedanceInfo.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { getGroupData, updateGroupData } from "../api/api";
 import '../Styles/attendance.css'
 import myImg from '../img/logo.png';
